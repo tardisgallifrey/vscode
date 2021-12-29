774add3181fc19e25251f126a4fe22da1c08bdd0
Dave <bigmanrunning@gmail.com>
2021-12-29 14:18:06 -0600

    second namespace 12/29/2021

---
+++ cplus/name_space/class2.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+namespace second {
+
+    class SecondClass {
+        public:
+           void display(){
+              cout << "Hello from inside namespace Second." << endl;
+            }  
+    };
+
+}
--- cplus/name_space/file1.cpp
+++ cplus/name_space/file1.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include "class1.cpp"
+#include "class2.cpp"
 
 using namespace std;
+using namespace second;
 
 int main(){
 
+    //Run our main code
     cout << "Hello World, Main File." << endl;
+
+    //Instantiate our class from first namespace
+    //and call it
     first::FirstClass obj;
     obj.display();
+
+    //Instantiate our class from second namespace
+    //and call it
+    SecondClass obj2;
+    obj2.display();
 }
