eb3b4b93e606c978dfbfc8b34406ccc7acb31def
Dave <bigmanrunning@gmail.com>
2021-12-29 13:56:53 -0600

    Next file 12/29/2021

---
+++ cplus/name_space/class1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+namespace First {
+
+    class FirstClass {
+        void display(){
+            cout << "Hello from inside namespace First." << endl;
+        }
+    }
+
+}
