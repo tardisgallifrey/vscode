8f06ca39e02702663a0ee43cd38d39f83983bfea
Dave <bigmanrunning@gmail.com>
2021-12-29 11:26:55 -0600

    threads 12/29/2021

---
+++ cplus/threads/thread.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+
+//This is the first one that needs a different library to compile
+//use:
+//  g++ thread.cpp -lpthread
+
+using namespace std;
+
+#define NUM_THREADS 5
+
+void *PrintHello(void *threadid) {
+   long tid;
+   tid = (long)threadid;
+   cout << "Hello World! Thread ID, " << tid << endl;
+   pthread_exit(NULL);
+}
+
+int main () {
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   int i;
+   
+   for( i = 0; i < NUM_THREADS; i++ ) {
+      cout << "main() : creating thread, " << i << endl;
+      rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+      
+      if (rc) {
+         cout << "Error:unable to create thread," << rc << endl;
+         exit(-1);
+      }
+   }
+   pthread_exit(NULL);
+}
