a174a826f9b63a35e2227fd48dfd5d287d690aed
Dave <bigmanrunning@gmail.com>
2021-12-30 10:45:07 -0600

    cmake step 2 12/30/2021

--- cfiles/hello.c
+++ cfiles/hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "TutorialConfig.h.in"
 
 
-int main(){
+int main(int argc, char* argv){
+
+    if (argc < 2) {
+    // report version
+    printf("%s Version %s . %s \n",argv[0],Tutorial_VERSION_MAJOR,Tutorial_VERSION_MINOR);
+    printf("Usage: %s \n", argv[0]);
+    return 1;
+    }
+
     printf("Hello World."\n);
+    return 0;
 }
