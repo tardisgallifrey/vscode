e2c32c3b0f4edb2b307ccb6c4b4b159ce66a6b5b
Dave <bigmanrunning@gmail.com>
2021-12-30 11:05:28 -0600

    Fixes 12/30/2021

--- cfiles/hello.c
+++ cfiles/hello.c
@@ -2,13 +2,12 @@
 #include "TutorialConfig.h"
 
 
-int main(int argc, char* argv){
+int main(int argc, char *argv[]){
 
     if (argc < 2) {
-    // report version
-    printf("%s Version %d.%d \n",argv[0],Tutorial_VERSION_MAJOR,Tutorial_VERSION_MINOR);
+    /*report version*/
+    printf("%s Version %d . %d \n", argv[0],Tutorial_VERSION_MAJOR,Tutorial_VERSION_MINOR);
     printf("Usage: %s \n", argv[0]);
-    return 1;
     }
 
     printf("Hello World.\n");
