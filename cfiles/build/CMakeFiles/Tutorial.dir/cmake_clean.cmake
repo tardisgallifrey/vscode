5e643a006d6eb1a7f3aaf7de410ecf74223bb2ad
Dave <bigmanrunning@gmail.com>
2021-12-30 10:46:44 -0600

    local update 12/30/2021

---
+++ cfiles/build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/hello.c.o"
+  "CMakeFiles/Tutorial.dir/hello.c.o.d"
+  "Tutorial"
+  "Tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
