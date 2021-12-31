dac9a6120bbb82f37cb85fba8a2480b2d5c9f9f2
Dave <bigmanrunning@gmail.com>
2021-12-31 10:28:28 -0600

    Class correction to Puppy 12/31/2021

--- java/src/Puppy.java
+++ java/src/Puppy.java
@@ -1,6 +1,10 @@
 package com.tardisgallifrey;
 
+//This is an example of constructor use
+
 public class Puppy {
+   //This is the constructor, 
+   //a method with the same name as the class name
    public Puppy(String name) {
       // This constructor has one parameter, name.
       System.out.println("Passed Name is :" + name );
@@ -8,6 +12,9 @@ public class Puppy {
 
    public static void main(String []args) {
       // Following statement would create an object myPuppy
+      //
+      //This type of constructor requires?? the paramter
+      //to be given upon instantiation
       Puppy myPuppy = new Puppy( "tommy" );
    }
 }
