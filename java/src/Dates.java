c0435972fc4c7bfd43449fed573382562d56a4ed
Dave <bigmanrunning@gmail.com>
2021-12-30 19:24:24 -0600

    Date class 12/30/2021

---
+++ java/src/Dates.java
@@ -0,0 +1,11 @@
+package com.tardisgallifrey;
+
+import java.util.Date; //Needed to work with Date variables
+
+public class Dates{
+    public static void Main(String[] args){
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
