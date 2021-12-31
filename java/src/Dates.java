fc726cc898549a0e8ed359d135774d1262ff71fb
Dave <bigmanrunning@gmail.com>
2021-12-30 19:30:12 -0600

    mistake 12/30/2021

--- java/src/Dates.java
+++ java/src/Dates.java
@@ -3,7 +3,7 @@ package com.tardisgallifrey;
 import java.util.Date; //Needed to work with Date variables
 
 public class Dates{
-    public static void Main(String[] args){
+    public static void main(String[] args){
         byte age = 30;
         Date now = new Date();
         System.out.println(now);
