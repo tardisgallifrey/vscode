b3d8663c82865a7e6ad18ae84b53a63b4066af09
Dave <bigmanrunning@gmail.com>
2021-12-31 12:40:27 -0600

    employee 12/31/2021

---
+++ java/src/classesinfiles/EmployeeTest.java
@@ -0,0 +1,25 @@
+package com.tardisgallifrey.employee;
+
+import java.io.*;
+
+//The class name must match file name
+//but, class can contain main function method
+public class EmployeeTest {
+
+   public static void main(String args[]) {
+      /* Create two objects using constructor */
+      Employee empOne = new Employee("James Smith");
+      Employee empTwo = new Employee("Mary Anne");
+
+      // Invoking methods for each object created
+      empOne.empAge(26);
+      empOne.empDesignation("Senior Software Engineer");
+      empOne.empSalary(1000);
+      empOne.printEmployee();
+
+      empTwo.empAge(21);
+      empTwo.empDesignation("Software Engineer");
+      empTwo.empSalary(500);
+      empTwo.printEmployee();
+   }
+}
