89978bc16ecaa940aacd1765ca6aab3b8a735584
Dave <bigmanrunning@gmail.com>
2022-01-01 15:17:04 -0600

    DateClass 01/01/2022

---
+++ java/src/DateCLass.java
@@ -0,0 +1,15 @@
+package com.tardisgallfrey;
+
+import java.util.Date;  //required
+
+public class DateClass {
+
+   public static void main(String args[]) {
+      // Instantiate a Date object
+      // date contains today
+      Date date = new Date();
+
+      // display time and date using toString()
+      System.out.println(date.toString());
+   }
+}
