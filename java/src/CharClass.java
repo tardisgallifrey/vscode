a09b20543f33c27995fa5557021ac3a32a360e48
Dave <bigmanrunning@gmail.com>
2022-01-01 14:32:03 -0600

    Char Objects 01/02/2022

---
+++ java/src/CharClass.java
@@ -0,0 +1,40 @@
+package com.tardisgallifrey;
+
+public class CharClass{
+
+    public static void main(){
+        //Character objects follow the same rules as 
+        //Number objects
+
+        //char primitive
+        char value = 'a';
+
+        //Char object
+        Char newValue = 'b';
+
+
+        System.out.println(newValue.isLetter());
+            //Determines whether the specified char value is a letter.
+
+        System.out.println(newValue.isDigit());
+            //Determines whether the specified char value is a digit.
+
+        //newValue.isWhitespace()
+            //Determines whether the specified char value is white space.
+
+        //newValue.isUpperCase()
+            //Determines whether the specified char value is uppercase.
+
+        //newValue.isLowerCase()
+            //Determines whether the specified char value is lowercase.
+
+        //newValue.toUpperCase()
+            //Returns the uppercase form of the specified char value.
+
+        //newValue.toLowerCase()
+            //Returns the lowercase form of the specified char value.
+
+        //newValue.toString()
+
+    }
+}
