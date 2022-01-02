acc0ec1c308a32ca9b0773b09568d1610268988f
Dave <bigmanrunning@gmail.com>
2022-01-02 12:45:45 -0600

    Thread update 01/02/2022

--- java/src/sockets/GreetingClient.java
+++ java/src/sockets/GreetingClient.java
@@ -7,26 +7,52 @@ import java.io.*;  //required
 //A java socket client
 //Server is on R-PI
 
+
+//The Client isn't a thread, just a regular class
 public class GreetingClient {
 
    public static void main(String [] args) {
+
+      //get serverName from command line
+      //It can be localhost or an IP address
       String serverName = args[0];
       int port = Integer.parseInt(args[1]);
+
+      //Run a try catch loop
       try {
          System.out.println("Connecting to " + serverName + " on port " + port);
+
+         //Instantiate a new socket variable with our data
          Socket client = new Socket(serverName, port);
          
+
+         //write a CONNECT message that we get from the remote socket
          System.out.println("Just connected to " + client.getRemoteSocketAddress());
+
+         //Open an OutputStream variable and stuff our output to the remote server in it
          OutputStream outToServer = client.getOutputStream();
+
+         //Instantiate a new DataOutputStream and construct with our
+         //output data
          DataOutputStream out = new DataOutputStream(outToServer);
          
+         //Write our output message to remote server
          out.writeUTF("Hello from " + client.getLocalSocketAddress());
+
+         //create new input stream and get what the client sends back
          InputStream inFromServer = client.getInputStream();
+
+         //Open a new DataInputStream and construct with Input Stream
          DataInputStream in = new DataInputStream(inFromServer);
          
+         //Print out what the server says
          System.out.println("Server says " + in.readUTF());
+
+         //Close connection
          client.close();
+
       } catch (IOException e) {
+         //handle error
          e.printStackTrace();
       }
    }
--- java/src/sockets/GreetingServer.java
+++ java/src/sockets/GreetingServer.java
@@ -4,17 +4,23 @@ package com.tardisgallifrey;
 import java.net.*;
 import java.io.*;
 
+//Our main class is a thread
+//We build a socket inside of it
 public class GreetingServer extends Thread {
    private ServerSocket serverSocket;
    
    public GreetingServer(int port) throws IOException {
+
+      //Here, we instantiate a new socket and assign a port
       serverSocket = new ServerSocket(port);
 
       //Used to ensure server shuts down
       serverSocket.setSoTimeout(100000);
    }
 
+   //This is a built in method for thread, not socket
    public void run() {
+      //Create endless loop
       while(true) {
          try {
             System.out.println("Waiting for client on port " + 
@@ -22,18 +28,31 @@ public class GreetingServer extends Thread {
             Socket server = serverSocket.accept();
             
             System.out.println("Just connected to " + server.getRemoteSocketAddress());
+
+            //Instantiate a new Data Input Stream, not from socket
             DataInputStream in = new DataInputStream(server.getInputStream());
             
+            //Readt the input stream with the readUTF method and print it
             System.out.println(in.readUTF());
+
+            //Instantiate a new Data Ouptut Stream to write back to the client
             DataOutputStream out = new DataOutputStream(server.getOutputStream());
+
+            //Send out the message
             out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
                + "\nGoodbye!");
+
+            //Close socket   
             server.close();
             
          } catch (SocketTimeoutException s) {
+
+            //Catch time out error when done
             System.out.println("Socket timed out!");
             break;
          } catch (IOException e) {
+
+            //Catch I/O error and print usual error trace
             e.printStackTrace();
             break;
          }
@@ -41,7 +60,12 @@ public class GreetingServer extends Thread {
    }
    
    public static void main(String [] args) {
+
+      //Get the port from command line, parse into int
       int port = Integer.parseInt(args[0]);
+
+      //Try to start() the thread after instantiation of a 
+      //new GreetingServer thread
       try {
          Thread t = new GreetingServer(port);
          t.start();
