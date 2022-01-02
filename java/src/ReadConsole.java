82d2812efc071f87e56c0230878a3f53afbdbe5f
Dave <bigmanrunning@gmail.com>
2022-01-02 10:50:25 -0600

    ReadConsole 01/02/2022

---
+++ java/src/ReadConsole.java
@@ -0,0 +1,27 @@
+package com.tardisgallifrey;
+
+import java.io.*; //required
+
+//Not often useful, but to see that
+//STDIN, STDOUT, and STDERR are streams, too.
+//
+public class ReadConsole {
+
+   public static void main(String args[]) throws IOException {
+      InputStreamReader cin = null;
+
+      try {
+         cin = new InputStreamReader(System.in);
+         System.out.println("Enter characters, 'q' to quit.");
+         char c;
+         do {
+            c = (char) cin.read();
+            System.out.print(c);
+         } while(c != 'q');
+      }finally {
+         if (cin != null) {
+            cin.close();
+         }
+      }
+   }
+}
